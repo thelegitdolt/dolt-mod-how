bd9d815fbb41f98b5c58dfdeaeb6260b1945b250
TheLegitDolt <asphaltpotatofy@gmail.com>
2023-05-15 22:55:46 -0700

    Bounding (it works)

--- src/main/java/com/dolthhaven/dolt_mod_how/core/other/DoltModHowEvent.java
+++ src/main/java/com/dolthhaven/dolt_mod_how/core/other/DoltModHowEvent.java
@@ -1,23 +1,34 @@
 package com.dolthhaven.dolt_mod_how.core.other;
 
+import com.dolthhaven.dolt_mod_how.core.DMHEnchants;
 import com.dolthhaven.dolt_mod_how.core.DoltModHow;
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.HoeItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
 import vectorwing.farmersdelight.common.registry.ModBlocks;
+
+import java.util.List;
+import java.util.Objects;
 
 @Mod.EventBusSubscriber(modid = DoltModHow.MOD_ID)
 public class DoltModHowEvent {
+
     @SubscribeEvent
     public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
         ItemStack stack = event.getItemStack();
@@ -44,5 +55,39 @@ public class DoltModHowEvent {
                 event.setCanceled(true);
             }
         }
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public static void onPlayerDropEvent(LivingDropsEvent event) {
+        if (event.getEntity() instanceof Player player) {
+            if (player.level.getGameRules().getBoolean(GameRules.RULE_KEEPINVENTORY)) {
+                return;
+            }
+
+            event.getDrops().stream().map(ItemEntity::getItem).filter(i -> i.getEnchantmentLevel(DMHEnchants.BOUNDING.get()) > 0).forEach(i -> player.getInventory().add(i));
+        }
+    }
+
+    // Some credit to team Cofh's soulbound effect
+    @SubscribeEvent
+    public static void onPlayerCloneEvent(PlayerEvent.Clone event) {
+        if (event.isWasDeath()) {
+            Player nw = event.getEntity();
+            Player old = event.getOriginal();
+
+            List<ItemStack> items = old.getInventory().items;
+
+            for (ItemStack i : items) {
+                DoltModHow.LOGGER.info(Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(i.getItem())).getPath() +" is " + i.getEnchantmentLevel(DMHEnchants.BOUNDING.get()));
+            }
+
+
+            for (ItemStack stackie : items.stream().filter(i -> i.getEnchantmentLevel(DMHEnchants.BOUNDING.get()) >= 1).toList()) {
+                if (!nw.getInventory().add(stackie)) {
+                    nw.drop(stackie, false);
+                }
+            }
+        }
+
     }
 }

42d11589fe7440b65b49e0e08e72a7be6cb88069
TheLegitDolt <asphaltpotatofy@gmail.com>
2023-07-21 12:18:45 -0700

    server level after i server their level (they don't like server level anymore)

--- src/main/java/com/dolthhaven/dolt_mod_how/core/mixin/MobEntityMixin.java
+++ src/main/java/com/dolthhaven/dolt_mod_how/core/mixin/MobEntityMixin.java
@@ -1,9 +1,7 @@
 package com.dolthhaven.dolt_mod_how.core.mixin;
 
-import com.dolthhaven.dolt_mod_how.core.DoltModHow;
-import com.teamabnormals.blueprint.core.util.MathUtil;
 import com.teamabnormals.environmental.core.registry.EnvironmentalItems;
-import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.EntityType;
@@ -12,6 +10,7 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -33,18 +32,20 @@ public abstract class MobEntityMixin extends LivingEntity {
 
     @Inject(method = "populateDefaultEquipmentSlots(Lnet/minecraft/util/RandomSource;Lnet/minecraft/world/DifficultyInstance;)V", at = @At("TAIL"))
     private void DoltModHow$ChanceForWanderingBootsSpawn(RandomSource random, DifficultyInstance difficulty, CallbackInfo info) {
-
-        double distance = Math.sqrt((this.getOnPos().getX() * this.getOnPos().getX()) + (this.getOnPos().getZ() * this.getOnPos().getZ()));
-        double chance = dubs(distance);
-        if (random.nextDouble() < chance) {
-            this.setItemSlot(EquipmentSlot.FEET, new ItemStack(EnvironmentalItems.WANDERER_BOOTS.get()));
-            this.armorDropChances[EquipmentSlot.FEET.getIndex()] = 1.0F;
+        if (this.getCommandSenderWorld() instanceof ServerLevel) {
+            this.position().distanceTo(new Vec3(0, 0, 0));
+            double distance = Math.sqrt((this.getOnPos().getX() * this.getOnPos().getX()) + (this.getOnPos().getZ() * this.getOnPos().getZ()));
+            double chance = dubs(distance);
+            if (random.nextDouble() < chance) {
+                this.setItemSlot(EquipmentSlot.FEET, new ItemStack(EnvironmentalItems.WANDERER_BOOTS.get()));
+                this.armorDropChances[EquipmentSlot.FEET.getIndex()] = 1.0F;
+            }
         }
     }
 
     private static double dubs(double distance) {
-        return (Math.pow((distance / 4500.0), 0.333) / 30.0) + (Math.exp(
-                - Math.pow((distance - 24000) / 8000.0, 2)) / 20.0);
+        return Math.min(1, Math.pow((distance / 4500.0), 0.333) / 30.0 + (Math.exp(
+                - Math.pow((distance - 24000) / 8000.0, 2)) / 20.0));
     }
 
 }

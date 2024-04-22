0a733f6bb30be41140b87bce69f03658b6d6d442
thelegitdolt <asphaltpotatofy@gmail.com>
2024-04-21 18:08:17 -0700

    leather scraps (thanks nive)

---
+++ src/main/java/com/dolthhaven/dolt_mod_how/core/registry/DMHItems.java
@@ -0,0 +1,17 @@
+package com.dolthhaven.dolt_mod_how.core.registry;
+
+import com.dolthhaven.dolt_mod_how.core.DoltModHow;
+import com.teamabnormals.blueprint.core.util.registry.ItemSubRegistryHelper;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.RegistryObject;
+
+@Mod.EventBusSubscriber(modid = DoltModHow.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DMHItems {
+    public static final ItemSubRegistryHelper HELPER = DoltModHow.REGISTRY_HELPER.getItemSubHelper();
+
+    public static final RegistryObject<Item> LEATHER_SCRAPS = HELPER.createItem("leather_scrap",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+
+}

d3a00d4582b40128412c9b0fdc29563cc48f17f1
thelegitdolt <asphaltpotatofy@gmail.com>
2025-02-08 23:17:57 -0800

    fix

--- src/main/java/com/dolthhaven/dolt_mod_how/core/other/DoltModHowDataUtil.java
+++ src/main/java/com/dolthhaven/dolt_mod_how/core/other/DoltModHowDataUtil.java
@@ -1,7 +1,6 @@
 package com.dolthhaven.dolt_mod_how.core.other;
 
 import com.dolthhaven.dolt_mod_how.core.util.Util;
-import com.ninni.etcetera.Etcetera;
 import com.teamabnormals.blueprint.core.util.DataUtil;
 import it.unimi.dsi.fastutil.objects.Object2FloatMap;
 import it.unimi.dsi.fastutil.objects.Object2FloatOpenHashMap;

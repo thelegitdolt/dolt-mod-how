12b040c48d5b43e6e26b37362e37ba20fcc28cb8
thelegitdolt <asphaltpotatofy@gmail.com>
2025-03-06 14:50:39 -0800

    test

--- src/main/java/com/dolthhaven/dolt_mod_how/data/tag/DMHBlockTags.java
+++ src/main/java/com/dolthhaven/dolt_mod_how/data/tag/DMHBlockTags.java
@@ -44,10 +44,8 @@ public class DMHBlockTags extends BlockTagsProvider {
 
         this.tag(BlockTags.FLOWER_POTS).add(POTTED_BEACHGRASS.get(), POTTED_MYCELIUM_SPROUTS.get(),
                 POTTED_TALL_BEACHGRASS.get(), POTTED_ARID_SPROUTS.get());
-
         this.tag(DMHTags.RARE_ORES).addTags(BlockTags.GOLD_ORES)
                 .addOptionalTag(new ResourceLocation("forge", "ores/silver"));
-
         this.tag(DMHTags.COMMON_ORES).add(AMETHYST_CLUSTER).addTags(BlockTags.IRON_ORES, BlockTags.COPPER_ORES)
                 .addOptionalTag(new ResourceLocation("forge", "ores/jade"))
                 .addOptionalTag(new ResourceLocation("forge", "ores/lead"))

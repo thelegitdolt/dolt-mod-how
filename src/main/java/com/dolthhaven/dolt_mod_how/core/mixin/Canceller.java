d984834e6e376c9ff2ebc1f41182266f41299bfb
thelegitdolt <asphaltpotatofy@gmail.com>
2025-05-08 16:17:04 -0700

    fixed abyssal chasm crash!

---
+++ src/main/java/com/dolthhaven/dolt_mod_how/core/mixin/Canceller.java
@@ -0,0 +1,12 @@
+package com.dolthhaven.dolt_mod_how.core.mixin;
+
+import com.bawnorton.mixinsquared.api.MixinCanceller;
+
+import java.util.List;
+
+public class Canceller implements MixinCanceller {
+    @Override
+    public boolean shouldCancel(List<String> targetClassNames, String mixinClassName) {
+        return mixinClassName.equals("com.ninni.etcetera.mixin.AbstractBlockStateMixin");
+    }
+}

d572aafd7adcfcd2d3e55a15b90c15d37a0df81c
thelegitdolt <asphaltpotatofy@gmail.com>
2025-11-18 00:26:23 -0800

    pretty sure this won't work

---
+++ src/main/java/com/dolthhaven/dolt_mod_how/core/other/events/DMHMappingEvents.java
@@ -0,0 +1,54 @@
+package com.dolthhaven.dolt_mod_how.core.other.events;
+
+import com.dolthhaven.dolt_mod_how.core.DoltModHow;
+import com.dolthhaven.dolt_mod_how.core.util.DMHUtils;
+import com.google.common.collect.ImmutableMap;
+import net.minecraft.Util;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.MissingMappingsEvent;
+
+import java.util.List;
+
+@Mod.EventBusSubscriber(modid = DoltModHow.MOD_ID)
+public class DMHMappingEvents {
+    @SubscribeEvent
+    public static void mappingsEvent(MissingMappingsEvent event) {
+        List<MissingMappingsEvent.Mapping<Block>> mappings = event.getAllMappings(ForgeRegistries.Keys.BLOCKS);
+
+        ImmutableMap<ResourceLocation, Block> mappers = Util.make(new ImmutableMap.Builder<ResourceLocation, Block>(), map -> {
+            putStuff(map, "stoneveil:stone_bricks_tiles", "caverns_and_chasms:cobblestone_bricks");
+            putStuff(map, "stoneveil:stone_bricks_tile_stairs", "caverns_and_chasms:cobblestone_brick_stairs");
+            putStuff(map, "stoneveil:stone_bricks_tile_slab", "caverns_and_chasms:cobblestone_brick_slab");
+            putStuff(map, "stoneveil:cracked_stone_bricks_tiles", "caverns_and_chasms:cobblestone_bricks");
+            putStuff(map, "stoneveil:cracked_stone_bricks_tile_stairs", "caverns_and_chasms:cobblestone_brick_stairs");
+            putStuff(map, "stoneveil:cracked_stone_bricks_tile_slab", "caverns_and_chasms:cobblestone_brick_slab");
+            putStuff(map, "stoneveil:mossy_stone_bricks_tiles", "caverns_and_chasms:mossy_cobblestone_bricks");
+            putStuff(map, "stoneveil:mossy_stone_bricks_tile_stairs", "caverns_and_chasms:mossy_cobblestone_brick_stairs");
+            putStuff(map, "stoneveil:mossy_stone_bricks_tile_slab", "caverns_and_chasms:mossy_cobblestone_brick_slab");
+
+            putStuff(map, "v_slab_compat:stoneveil/cracked_stone_bricks_tile_vertical_slab", "v_slab_compat:caverns_and_chasms/cobblestone_brick_vertical_slab");
+            putStuff(map, "v_slab_compat:stoneveil/mossy_stone_bricks_tile_vertical_slab", "v_slab_compat:caverns_and_chasms/mossy_cobblestone_brick_vertical_slab");
+            putStuff(map, "v_slab_compat:stoneveil/stone_bricks_tile_vertical_slab",  "v_slab_compat:caverns_and_chasms/cobblestone_brick_vertical_slab");
+            putStuff(map, "stoneveil:stone_pilar", "architects_palette:tuff_pillar");
+        }).build();
+
+        for (MissingMappingsEvent.Mapping<Block> mapping : mappings) {
+            Block block =mappers.get(mapping.getKey());
+            if (block != null) {
+                if (ForgeRegistries.BLOCKS.getKey(block) != null) {
+                    mapping.remap(block);
+                }
+            }
+        }
+    }
+
+    private static void putStuff(ImmutableMap.Builder<ResourceLocation, Block> map, String a, String b) {
+        Block block = DMHUtils.getPotentialBlock(new ResourceLocation(b));
+        if (block != null)
+            map.put(new ResourceLocation(a), block);
+    }
+}
--- src/main/java/com/dolthhaven/dolt_mod_how/integration/emi/DMHEmiPlugin.java
+++ src/main/java/com/dolthhaven/dolt_mod_how/integration/emi/DMHEmiPlugin.java
@@ -1,5 +1,7 @@
 package com.dolthhaven.dolt_mod_how.integration.emi;
 
+import com.dolthhaven.dolt_mod_how.core.DMHConfig;
+import com.dolthhaven.dolt_mod_how.core.util.DMHUtils;
 import dev.emi.emi.api.EmiEntrypoint;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
@@ -22,39 +24,42 @@ public class DMHEmiPlugin implements EmiPlugin {
     @Override
     public void register(EmiRegistry registry) {
         registry.addRecipeHandler(BackpackModule.menyType, new BackpackScreenHandler());
+        if (DMHConfig.CLIENT.hidePotions.get()) {
+            registry.removeEmiStacks(a -> {
+                ItemStack stack = a.getItemStack();
+                if (stack.isEmpty()) return false;
+                CompoundTag tag = stack.getTag();
+                if (tag == null) return false;
 
-        registry.removeEmiStacks(a -> {
-            ItemStack stack = a.getItemStack();
-            if (stack.isEmpty()) return false;
-            CompoundTag tag = stack.getTag();
-            if (tag == null) return false;
+                Potion potion = PotionUtils.getPotion(tag);
 
-            Potion potion = PotionUtils.getPotion(tag);
+                // hides all subtle potions that aren't normal water potions
+                boolean subtle = tag.getBoolean("Subtle");
+                if (subtle && potion == Potions.WATER && stack.getItem() != Items.POTION) return true;
 
-            // hides all subtle potions that aren't normal water potions
-            boolean subtle = tag.getBoolean("Subtle");
-            if (subtle && potion == Potions.WATER && stack.getItem() != Items.POTION) return true;
+                // do not hide non-potions and water potions
+                if (potion == Potions.EMPTY || potion == Potions.WATER) return false;
 
-            // do not hide non-potions and water potions
-            if (potion == Potions.EMPTY || potion == Potions.WATER) return false;
+                    // hide all non-water variant potions
+                else if (stack.getItem() != Items.POTION) return true;
 
-            // hide all non-water variant potions
-            else if (stack.getItem() != Items.POTION) return true;
+                String potionType = tag.getString("Potion").toLowerCase(Locale.ROOT);
 
-            String potionType = tag.getString("Potion").toLowerCase(Locale.ROOT);
+                // hides all normal potions that are long and strong and subtle variants
+                if (potionType.contains("long") || potionType.contains("strong") || subtle) return true;
 
-            // hides all normal potions that are long and strong and subtle variants
-            if (potionType.contains("long") || potionType.contains("strong") || subtle) return true;
+                return false;
+            });
+        }
 
-            return false;
-        });
+        if (DMHConfig.CLIENT.hideEnchants.get()) {
+            registry.removeEmiStacks(a -> {
+                ItemStack stack = a.getItemStack();
+                if (stack.isEmpty() || stack.getItem() != Items.ENCHANTED_BOOK) return false;
 
-        registry.removeEmiStacks(a -> {
-            ItemStack stack = a.getItemStack();
-            if (stack.isEmpty() || stack.getItem() != Items.ENCHANTED_BOOK) return false;
-
-            Map.Entry<Enchantment, Integer> enchant = EnchantmentHelper.getEnchantments(stack).entrySet().iterator().next();
-            return enchant.getValue() < enchant.getKey().getMaxLevel();
-        });
+                Map.Entry<Enchantment, Integer> enchant = EnchantmentHelper.getEnchantments(stack).entrySet().iterator().next();
+                return enchant.getValue() < enchant.getKey().getMaxLevel();
+            });
+        }
     }
 }

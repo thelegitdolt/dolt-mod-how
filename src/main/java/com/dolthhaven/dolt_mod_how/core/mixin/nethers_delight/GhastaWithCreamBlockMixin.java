c799fbc1e3b7c8fafdb50ed8b49d350fb48f2a56
thelegitdolt <asphaltpotatofy@gmail.com>
2026-03-11 20:53:29 -0700

    ghasta with cream no longer regenerate

---
+++ src/main/java/com/dolthhaven/dolt_mod_how/core/mixin/nethers_delight/GhastaWithCreamBlockMixin.java
@@ -0,0 +1,16 @@
+package com.dolthhaven.dolt_mod_how.core.mixin.nethers_delight;
+
+import com.soytutta.mynethersdelight.common.block.GhastaWithCreamBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(GhastaWithCreamBlock.class)
+public class GhastaWithCreamBlockMixin {
+    @Inject(method = "isRandomlyTicking", at = @At("HEAD"), cancellable = true)
+    private void sex(BlockState state, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(false);
+    }
+}

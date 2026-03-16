4e41f807bff6eed8f80c2e04474c8517d37a9f71
thelegitdolt <asphaltpotatofy@gmail.com>
2026-03-15 20:48:04 -0700

    advancement fixes

--- src/main/java/com/dolthhaven/dolt_mod_how/data/DMHAdvancements.java
+++ src/main/java/com/dolthhaven/dolt_mod_how/data/DMHAdvancements.java
@@ -1,17 +1,17 @@
 package com.dolthhaven.dolt_mod_how.data;
 
-import com.dolthhaven.dolt_mod_how.common.WatchMobKillTrigger;
 import com.dolthhaven.dolt_mod_how.core.DoltModHow;
 import com.dolthhaven.dolt_mod_how.core.registry.DMHCriteriaTriggers;
 import com.teamabnormals.caverns_and_chasms.core.registry.CCItems;
+import com.teamabnormals.upgrade_aquatic.core.registry.UAItems;
 import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementRewards;
 import net.minecraft.advancements.FrameType;
-import net.minecraft.advancements.critereon.ContextAwarePredicate;
+import net.minecraft.commands.CommandFunction;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.storage.loot.predicates.LootItemEntityPropertyCondition;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.common.data.ForgeAdvancementProvider;
 import net.minecraftforge.data.event.GatherDataEvent;
@@ -28,15 +28,18 @@ public class DMHAdvancements implements ForgeAdvancementProvider.AdvancementGene
 
     @Override
     public void generate(HolderLookup.Provider registries, Consumer<Advancement> saver, ExistingFileHelper existingFileHelper) {
-        adv(new ResourceLocation("adventure/totem_of_undying"), CCItems.SPINEL.get(), "adventure", "totes", FrameType.TASK, true)
+        adv(new ResourceLocation("adventure/totem_of_undying"), CCItems.SPINEL.get(), "adventure", "trigger_mime_totem", FrameType.TASK, true, 0)
                 .addCriterion("mimers", DMHCriteriaTriggers.TRIGGER_MIME_TOTEM.createInstance())
                 .save(saver, DoltModHow.MOD_ID + ":adventure/trigger_mime_totem");
+        adv(new ResourceLocation("nether/root"), UAItems.THRASHER_TOOTH.get(), "nether", "nether_thrasher", FrameType.CHALLENGE, true, 100)
+                .addCriterion("thrash", DMHCriteriaTriggers.NETHER_THRASHER.createInstance())
+                .save(saver, DoltModHow.MOD_ID + ":nether/nether_thrasher");
     }
 
-    public Advancement.Builder adv(ResourceLocation parent, Item item, String category, String name, FrameType frame, boolean hidden) {
+    public Advancement.Builder adv(ResourceLocation parent, Item item, String category, String name, FrameType frame, boolean hidden, int rewards) {
         return Advancement.Builder.advancement()
                 .parent(Advancement.Builder.advancement()
-                        .build(parent)).display(item,
+                        .build(parent)).rewards(new AdvancementRewards(rewards, new ResourceLocation[0], new ResourceLocation[0], CommandFunction.CacheableFunction.NONE)).display(item,
                         Component.translatable("advancements.dolt_mod_how." +
                                 category + "." + name + ".title"),
                         Component.translatable("advancements.dolt_mod_how." + category + "." + name + ".description"),

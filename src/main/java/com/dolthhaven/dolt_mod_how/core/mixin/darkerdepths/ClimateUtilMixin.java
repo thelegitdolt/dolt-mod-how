c13866c42b9d68847706c3adba092495dc1f975c
thelegitdolt <asphaltpotatofy@gmail.com>
2026-04-30 15:58:15 -0700

    uh

---
+++ src/main/java/com/dolthhaven/dolt_mod_how/core/mixin/darkerdepths/ClimateUtilMixin.java
@@ -0,0 +1,23 @@
+package com.dolthhaven.dolt_mod_how.core.mixin.darkerdepths;
+
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.mojang.datafixers.util.Pair;
+import com.naterbobber.darkerdepths.worldgen.ClimateParamsUtil;
+import net.minecraft.resources.ResourceKey;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.function.Consumer;
+
+@Mixin(ClimateParamsUtil.class)
+public class ClimateUtilMixin {
+    @WrapWithCondition(method = "createBiomes", at = @At(value = "INVOKE", target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V"), remap = false)
+    private static <T> boolean sex(Consumer<T> instance, T t) {
+        if (t instanceof Pair<?, ?> pair) {
+            if (pair.getSecond() instanceof ResourceKey<?> key) {
+                return !key.location().getPath().equals("glowshroom_forest");
+            }
+        }
+        return true;
+    }
+}
---
+++ src/main/java/com/dolthhaven/dolt_mod_how/core/mixin/what.java
@@ -0,0 +1,20 @@
+package com.dolthhaven.dolt_mod_how.core.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.ninni.spawn.registry.SpawnCreativeModeTab;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.loading.FMLLoader;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(SpawnCreativeModeTab.class)
+public class what {
+    @WrapOperation(method = "registerTabs", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/common/ForgeConfigSpec$BooleanValue;get()Ljava/lang/Object;"), remap = false)
+    private static Object sex(ForgeConfigSpec.BooleanValue instance, Operation<Object> original) {
+        if (FMLLoader.isProduction()) {
+            return true;
+        }
+        return original.call(instance);
+    }
+}

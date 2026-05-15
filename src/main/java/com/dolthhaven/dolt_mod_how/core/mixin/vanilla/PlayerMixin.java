4af87838a276cc057933fb65c3555d52cc0d6d84
thelegitdolt <asphaltpotatofy@gmail.com>
2026-05-14 21:28:26 -0700

    John Potion

---
+++ src/main/java/com/dolthhaven/dolt_mod_how/core/mixin/vanilla/PlayerMixin.java
@@ -0,0 +1,18 @@
+package com.dolthhaven.dolt_mod_how.core.mixin.vanilla;
+
+import com.dolthhaven.dolt_mod_how.core.registry.DMHMobEffects;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Player.class)
+public class PlayerMixin {
+    @Inject(method = "canEat", at = @At("HEAD"), cancellable = true)
+    private void DoltModHow$RapaciousAlwaysEat(boolean p_36392_, CallbackInfoReturnable<Boolean> cir) {
+        if (((Player)(Object) this).hasEffect(DMHMobEffects.RAPACITY.get())) {
+            cir.setReturnValue(true);
+        }
+    }
+}
--- src/main/java/com/dolthhaven/dolt_mod_how/core/registry/DMHMobEffects.java
+++ src/main/java/com/dolthhaven/dolt_mod_how/core/registry/DMHMobEffects.java
@@ -4,6 +4,7 @@ import com.dolthhaven.dolt_mod_how.common.effect.GenerosityMobEffect;
 import com.dolthhaven.dolt_mod_how.common.effect.MimingMobEffect;
 import com.dolthhaven.dolt_mod_how.core.DoltModHow;
 import com.dolthhaven.dolt_mod_how.core.util.DMHUtils;
+import com.teamabnormals.blueprint.common.effect.BlueprintMobEffect;
 import com.teamabnormals.blueprint.core.util.DataUtil;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
@@ -26,18 +27,23 @@ public class DMHMobEffects {
             new MimingMobEffect(MobEffectCategory.NEUTRAL));
     public static final RegistryObject<MobEffect> GENEROSITY = MOB_EFFECTS.register("generosity", () ->
             new GenerosityMobEffect(MobEffectCategory.NEUTRAL));
-
+    public static final RegistryObject<MobEffect> RAPACITY = MOB_EFFECTS.register("rapacity", () ->
+            new BlueprintMobEffect(MobEffectCategory.BENEFICIAL, 0xff9970));
 
 
     public static final RegistryObject<Potion> NORMAL_MIMING = POTIONS.register("miming",
             () -> new Potion("miming", new MobEffectInstance(MIMING.get())));
     public static final RegistryObject<Potion> MIMING_STRONG = POTIONS.register("strong_miming",
             () -> new Potion("miming", new MobEffectInstance(MIMING.get(), 0, 1)));
-
     public static final RegistryObject<Potion> NORMAL_GENEROSITY = POTIONS.register("generosity",
             () -> new Potion("generosity", new MobEffectInstance(GENEROSITY.get())));
     public static final RegistryObject<Potion> STRONG_GENEROSITY = POTIONS.register("strong_generosity",
             () -> new Potion("generosity", new MobEffectInstance(GENEROSITY.get(), 0, 1)));
+
+    public static final RegistryObject<Potion> NORMAL_RAPACITY = POTIONS.register("rapacity",
+            () -> new Potion("rapacity", new MobEffectInstance(RAPACITY.get(), 3600)));
+    public static final RegistryObject<Potion> LONG_RAPACITY = POTIONS.register("long_rapacity",
+            () -> new Potion("rapacity", new MobEffectInstance(RAPACITY.get(), 9600)));
 
     public static void registerBrewingRecipes() {
         Item item = DMHUtils.getPotentialItem(DMHUtils.Constants.ZIRCONIA);
@@ -51,6 +57,8 @@ public class DMHMobEffects {
         DataUtil.addMix(NORMAL_MIMING.get(), Items.FERMENTED_SPIDER_EYE, NORMAL_GENEROSITY.get());
         DataUtil.addMix(MIMING_STRONG.get(), Items.FERMENTED_SPIDER_EYE, STRONG_GENEROSITY.get());
 
+        DataUtil.addMix(Potions.AWKWARD, Items.ROTTEN_FLESH, NORMAL_RAPACITY.get());
+        DataUtil.addMix(NORMAL_RAPACITY.get(), Items.REDSTONE, LONG_RAPACITY.get());
     }
 
 }

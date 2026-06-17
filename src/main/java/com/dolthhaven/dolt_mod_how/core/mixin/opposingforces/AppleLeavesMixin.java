2c4e9fe4e2275bbf2934045a24a2f09dc2e6ab4e
thelegitdolt <asphaltpotatofy@gmail.com>
2026-06-16 23:30:38 -0700

    oops

--- src/main/java/com/dolthhaven/dolt_mod_how/core/mixin/opposingforces/AppleLeavesMixin.java
+++ src/main/java/com/dolthhaven/dolt_mod_how/core/mixin/opposingforces/AppleLeavesMixin.java
@@ -3,12 +3,13 @@ package com.dolthhaven.dolt_mod_how.core.mixin.opposingforces;
 import com.unusualmodding.opposing_force.blocks.FruitfulLeavesBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(FruitfulLeavesBlock.class)
 public class AppleLeavesMixin {
-    @ModifyArg(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;<init>(Lnet/minecraft/world/level/ItemLike;I)V", ordinal = 1))
-    private int DoltModHow$LeavesDropOneApple(int old) {
-        return 1;
+    @ModifyArgs(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;<init>(Lnet/minecraft/world/level/ItemLike;I)V"))
+    private void DoltModHow$LeavesDropOneApple(Args args) {
+        args.set(1, 1);
     }
 }
